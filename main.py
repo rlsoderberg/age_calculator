9ef9371734ba30c752fc9f13995ebb808aa1064b
rlsoderberg <vogindex@gmail.com>
2025-07-07 19:50:44 -0700

    provided intro text

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+print(">>>calculate when the 'half-your-age-plus-seven' rule becomes valid for a pair of humans")
+
 current_year = int(input('enter current year: '))
 younger_birth = int(input('enter younger birth year: '))
 older_birth = int(input('enter older birth year: '))
@@ -10,7 +12,6 @@ for x in range (0,50):
     if ((older_age - younger_age) + 7) <= younger_age:
         valid_years.append(current_year)
 
-print('valid years begin ' + str(valid_years[1]))
-
+print('rule becomes valid for these two humans in the year ' + str(valid_years[1]))
 
     
