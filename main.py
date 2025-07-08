eda910e54f60b6eaea3282755c21f059f9870848
rlsoderberg <vogindex@gmail.com>
2025-07-07 18:52:25 -0700

    updated to allow two inputs

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 current_year = 2025
-becca_birth = 1994
-subject_birth = int(input('enter subject birth year: '))
+younger_birth = int(input('enter younger birth year: '))
+older_birth = int(input('enter older birth year: '))
 valid_years = []
 
 
 for x in range (0,50):
-    becca_age = (current_year - becca_birth)
-    subject_age = (current_year - subject_birth)
+    younger_age = (current_year - younger_birth)
+    older_age = (current_year - older_birth)
     current_year += 1
     print('current year: '+str(current_year))
-    print('subject_age: '+str(subject_age))
-    print('becca_age: '+str(becca_age))
-    print('(subject_age - becca_age) + 7): '+str((subject_age - becca_age) + 7))
-    if ((subject_age - becca_age) + 7) <= becca_age:
+    print('older_age: '+str(older_age))
+    print('younger_age: '+str(younger_age))
+    print('(older_age - younger_age) + 7): '+str((older_age - younger_age) + 7))
+    if ((older_age - younger_age) + 7) <= younger_age:
         valid_years.append(current_year)
         print('valid year!')
     else:
