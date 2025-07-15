cfd90d71169f53d35d8c9f034ff018c203ae24a0
rlsoderberg <vogindex@gmail.com>
2025-07-14 18:31:25 -0700

    added a couple of age displays for usability

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-print("'half-your-age-plus-seven' calculator")
+print("half plus seven calculator")
 
 current_year = int(input('enter current year: '))
 younger_birth = int(input('enter younger birth year: '))
 older_birth = int(input('enter older birth year: '))
+print('in '+str(current_year)+', young is '+str(current_year-younger_birth)+' and old is '+str(current_year-older_birth)+ '\n')
 valid_years = []
 
 for x in range (0,50):
     younger_age = (current_year - younger_birth)
     older_age = (current_year - older_birth)
-    current_year += 1
-    if ((older_age - younger_age) + 7) <= younger_age:
+    if (( younger_age *2 )-14 >= older_age):
         valid_years.append(current_year)
+    current_year+=1
 
 print('rule becomes valid in ' + str(valid_years[1]))
+print('in '+str(valid_years[1])+', young is ' + str(valid_years[1]-younger_birth)+ ' and old is '+ str(valid_years[1]-older_birth))
 
-    
+
+
+
+
